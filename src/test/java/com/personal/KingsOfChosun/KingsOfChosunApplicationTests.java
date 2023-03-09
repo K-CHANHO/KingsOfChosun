5f6c1d3ef26da6ee940c150abeb5937ee59dbb4d
Chanho Kim <cksgh1565@gmail.com>
2023-03-09 22:37:07 +0900

    Initial commit

---
+++ src/test/java/com/personal/KingsOfChosun/KingsOfChosunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.KingsOfChosun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingsOfChosunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
